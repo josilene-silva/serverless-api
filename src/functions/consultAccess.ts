5181aa55493e22aeb24ee4168cbbf1f60930990c
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-02-03 19:53:37 -0400

    feat: create consultAccess function

---
+++ src/functions/consultAccess.ts
@@ -0,0 +1,33 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+import countapi from 'countapi-js';
+
+const NAMESPACE = process.env.COUNTAPI_NAMESPACE;
+const KEY = process.env.COUNTAPI_KEY;
+
+interface IConsult {
+    status: number,
+    path: string,
+    value: number,
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+    try {
+        const { value }: IConsult = await countapi.get(NAMESPACE, KEY);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                access: value
+            })
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Error on consult access",
+                error: error
+            })
+        }
+    }
+}
